11d4342a9a4e223399713fa944cb493dc7942b60
Tongrui Li <tli1998@utexas.edu>
2020-07-15 10:03:34 -0500

    made things work

--- keypress.py
+++ keypress.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python
-import getch
+#import getch
 import rospy
 from std_msgs.msg import String #String message 
 from std_msgs.msg import Int8
+from pynput.keyboard import Key, Listener
 
 
 ################################
 # created by yuvaram
 #yuvaramsingh94@gmail.com
-################################
+# modified by Tongrui Li
+################################\
+pub = rospy.Publisher('key',Int8,queue_size=10) # "key" is the publisher name
+rospy.init_node('keypress',anonymous=True)
+rate = rospy.Rate(20)#try removing this line ans see what happens
 
+def on_press(key):
 
-def keys():
-    pub = rospy.Publisher('key',Int8,queue_size=10) # "key" is the publisher name
-    rospy.init_node('keypress',anonymous=True)
-    rate = rospy.Rate(10)#try removing this line ans see what happens
-    while not rospy.is_shutdown():
-        k=ord(getch.getch())# this is used to convert the keypress event in the keyboard or joypad , joystick to a ord value
-        if ((k>=65)&(k<=68)|(k==115)|(k==113)|(k==97)):# to filter only the up , dowm ,left , right key /// this line can be removed or more key can be added to this
-            rospy.loginfo(str(k))# to print on  terminal 
-            pub.publish(k)#to publish
-        #rospy.loginfo(str(k))
+    if hasattr(key, "char") and key.char == "w":
+        print("Entering on press event for w")
+        rospy.loginfo(str(1))# to print on  terminal 
+        pub.publish(1)#to publish
+    print('{} pressed'.format(
+        key))
 
-        #rate.sleep()
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
 
-#s=115,e=101,g=103,b=98
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
 
-if __name__=='__main__':
-    try:
-        keys()
-    except rospy.ROSInterruptException:
-        pass
